86595ad6074ad2c17fcfca92f35c6fe8c1dfb3aa
Exequel <exql.adzn@gmail.com>
2023-02-24 12:32:17 +0800

    add explanation for sample problem

--- basic-js-challenges/challenge-2.js
+++ basic-js-challenges/challenge-2.js
@@ -2,8 +2,16 @@
 
 // ORIGINAL CODE:
 // const some Number = 20;
-// someNumber = 50;
+// someNumber = 50
+
 
 // REVISED CODE:
 let someNumber = 20;
 someNumber = 50;
+
+console.log(someNumber);    
+
+// EXPLANATION:
+// 1 remove space from some Number variable (variable names can't contain spaces)
+// 2 change const to let to be able to reassign value
+// 3 missing semi-colon at the end
