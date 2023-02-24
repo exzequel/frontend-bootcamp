fdb55540ab9b865981ca98920ebe750ba4478543
Exequel <exql.adzn@gmail.com>
2023-02-24 16:30:25 +0800

    add code challenge problem

--- learn-javascript/basic-js-operator-challenges/challenge-4.js
+++ learn-javascript/basic-js-operator-challenges/challenge-4.js
@@ -1,1 +1,12 @@
 // Challenge 4
+// What does result evaluate to?
+
+const myObj = {
+    prop1: 'first value',
+    prop2: 20
+};
+
+// Index         0   1   2
+const myArray = [40, 50, 2];
+
+const result = myObj.prop2 === (myArray[0] / myArray[2]);
