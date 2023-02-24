7372079586b971c4860cf5b67ece0701e32a73a9
Exequel <exql.adzn@gmail.com>
2023-02-24 14:15:01 +0800

    insert code problem in challenge-1

---
+++ learn-javascript/basic-js-operator-challenges/challenge-1.js
@@ -0,0 +1,11 @@
+// Challenge 1
+// Why doesn't this code work? Try to make it work and guess what the result will be
+
+// ORIGINAL CODE:
+const numberVariable = 0;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+
+console.log(numberVariable);
