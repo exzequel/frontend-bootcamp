cb808f7ed43427e08a1e44963f71d781729c2afd
Exequel <exql.adzn@gmail.com>
2023-02-24 16:37:42 +0800

    add challenge-5 problem

--- learn-javascript/basic-js-operator-challenges/challenge-5.js
+++ learn-javascript/basic-js-operator-challenges/challenge-5.js
@@ -1,1 +1,18 @@
 // Challenge 5
+// This one is meant to be a tough challenge. You probably will need to Google this and it might take you a while to complete.
+
+// What does result evaluate to ?
+const myObj = {
+    nestedObject1: {
+        price: 100,
+        quantity: 5
+    },
+    nestedObject2: {
+        price: 150,
+        quantity: 2
+    }
+};
+
+const myArray = [myObj.nestedObject1, myObj.nestedObject2];
+
+const result = (myArray[0].price * myArray[0].quantity) > (myArray[1].price * myArray[1].quantity); 
