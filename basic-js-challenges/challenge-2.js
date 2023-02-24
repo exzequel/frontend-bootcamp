452245aa1f9d7f2a2e7372e6ef85a79905cad78f
Exequel <exql.adzn@gmail.com>
2023-02-24 12:27:49 +0800

    add solution

--- basic-js-challenges/challenge-2.js
+++ basic-js-challenges/challenge-2.js
@@ -1,5 +1,9 @@
 // There are three things wrong with given code. Find and explain why they are wrong.
 
 // ORIGINAL CODE:
-const some Number = 20;
+// const some Number = 20;
+// someNumber = 50;
+
+// REVISED CODE:
+let someNumber = 20;
 someNumber = 50;
