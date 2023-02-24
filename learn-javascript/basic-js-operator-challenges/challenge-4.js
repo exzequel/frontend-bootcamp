844d3fcd17d6c41eb8fed804c21e02774f9bd93a
Exequel <exql.adzn@gmail.com>
2023-02-24 16:34:08 +0800

    add explanation

--- learn-javascript/basic-js-operator-challenges/challenge-4.js
+++ learn-javascript/basic-js-operator-challenges/challenge-4.js
@@ -9,7 +9,14 @@ const myObj = {
 // Index         0   1   2
 const myArray = [40, 50, 2];
 
-const result = myObj.prop2 === (myArray[0] / myArray[2]);
+const result = myObj.prop2 === (myArray[0] / myArray[2]); // true
 
 // ANSWER:
 // result will evaluate to true
+
+// EXPLANATION:
+// myObj.prop2 is 20
+// myArray[0] is 40
+// myArray[2] is 2
+// myArray[0] / myArray[2] is 20
+// 20 === 20 is true
