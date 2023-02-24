52b9fda6ee866bc73094ac5c94a4b9fcb274cd34
Exequel <exql.adzn@gmail.com>
2023-02-24 12:47:05 +0800

    add copy of original code

--- basic-js-challenges/challenge-4.js
+++ basic-js-challenges/challenge-4.js
@@ -1,9 +1,10 @@
 // Why is this code invalid, edit until it is valid.
 
-const objectVariable = {
-    property1: 'i am property 1';
-    property2: 'i am property 2';
-    property3: [20, 30, 40];    
-};
+// ORIGINAL CODE:
+// const objectVariable = {
+//     property1: 'i am property 1';
+//     property2: 'i am property 2';
+//     property3: [20, 30, 40];    
+// };
 
 console.log(objectVariable.property3[2]);
