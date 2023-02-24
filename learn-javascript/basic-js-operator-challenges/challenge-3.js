dd66fb263cc4d4730074609c8501487ede620955
Exequel <exql.adzn@gmail.com>
2023-02-24 16:28:00 +0800

    add explanation

--- learn-javascript/basic-js-operator-challenges/challenge-3.js
+++ learn-javascript/basic-js-operator-challenges/challenge-3.js
@@ -2,15 +2,17 @@
 // What does expression5 evaluate to? How could you write this in a single line of code,
 // (for exercise purposes only; you would never want to combine all this in one line)
 
-const expression1 = 100 % 50;
-const expression2 = 100 / 50;
-const expression3 = expression1 < expression2;
-const expression4 = expression3 && 300 + 5 === 305;
-const expression5 = !expression4;
+const expression1 = 100 % 50; // will output 0
+const expression2 = 100 / 50; // will output 2
+const expression3 = expression1 < expression2; // 0 < 2, evaluates to true
+const expression4 = expression3 && 300 + 5 === 305; // expression is true and 300 + 5 is strictly equal to 305, evaluates to true
+const expression5 = !expression4; // reverses expression 4, evaluates to false
 
 console.log(expression5);
 
 // SOLUTION: 
-const result = !(((100 % 50) < (100/50)) && 300 + 5 === 305);
+const result = !(((100 % 50) < (100/50)) && 300 + 5 === 305); // break down code into smaller chunks
 
 console.log(result);
+
+
