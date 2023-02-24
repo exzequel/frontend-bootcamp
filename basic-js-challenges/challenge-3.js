ea7c6f984f7242f56003dba5e04896c58668a5b5
Exequel <exql.adzn@gmail.com>
2023-02-24 12:43:21 +0800

    add and link a more comprehensive solution to problem

--- basic-js-challenges/challenge-3.js
+++ basic-js-challenges/challenge-3.js
@@ -9,3 +9,12 @@ console.log(resultVariable);
 
 // resultVariable = 2040
 // the result will output '2040' since variable2 is a string variable
+
+// comprehensive explanation: https://github.com/zachgoll/fullstack-roadmap-series/blob/main/code-challenges/lesson-3/challenge-3-solution.md
+
+// the resultVariable will output a string because js is a "weakly typed" language
+// since js is "weakly typed" you can do operations with multiple variable types at once
+
+// "implicit type coercion"
+// since we don't explicitly declare each variable's type (static typing)
+// in js when you add a string and number together, it automatically becomes a string
