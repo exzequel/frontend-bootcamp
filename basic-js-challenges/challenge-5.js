d81f85298d9a652fdab242289f9e6e276b63bbbc
Exequel <exql.adzn@gmail.com>
2023-02-24 12:57:32 +0800

    add alternate solution

--- basic-js-challenges/challenge-5.js
+++ basic-js-challenges/challenge-5.js
@@ -2,12 +2,21 @@
 
 // ORIGINAL CODE:
 // const myArray = [20, 30, 40];
+// console.log(myArray[3]);
 
+// REVISED CODE:
 const myArray = [20, 30, 40];
-
 console.log(myArray[2]);
 
 // EXPLANATION:
 // original code is invalid because there is no index 3 in given array
 // arrays start at index 0 so in order to correct this code
 // we can either call index 2 of the array or add another value to the array, and call index 3
+
+// ALTERNATE SOLUTION:
+
+const otherArray = [20, 30, 40, 50];
+// index 0, 1, 2, 3 = 20, 30, 40, 50
+
+console.log(otherArray[3]);
+// prints 50
