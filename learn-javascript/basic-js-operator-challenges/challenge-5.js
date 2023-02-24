c26083a3079fca16835e459d60f396b2164fa24a
Exequel <exql.adzn@gmail.com>
2023-02-24 16:44:18 +0800

    add explanation

--- learn-javascript/basic-js-operator-challenges/challenge-5.js
+++ learn-javascript/basic-js-operator-challenges/challenge-5.js
@@ -18,8 +18,13 @@ const myArray = [myObj.nestedObject1, myObj.nestedObject2];
 const result = (myArray[0].price * myArray[0].quantity) > (myArray[1].price * myArray[1].quantity); 
 
 // SOLUTION
-
 const resultBreakdown = (100 * 5) > (150 * 2);
 
 console.log(resultBreakdown);
 console.log(result);
+
+// EXPLANATION:
+// myArray consists of myObj.nestedObject1 (index 0) and myObj.nestedObject2 (index 1)
+// myArray[0].price is 100, myArray[0].quantity is 5, multiplying the two will result to 500
+// myArray[1].price is 150, myArray[1].quantity is 2, multiplying the two will result to 300
+// 500 > 300, therefore result will evaluate to true
