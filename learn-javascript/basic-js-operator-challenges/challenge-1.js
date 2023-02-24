571679775488ecb3d3f17a0143907312986818b0
Exequel <exql.adzn@gmail.com>
2023-02-24 16:07:49 +0800

    add solution

--- learn-javascript/basic-js-operator-challenges/challenge-1.js
+++ learn-javascript/basic-js-operator-challenges/challenge-1.js
@@ -2,10 +2,19 @@
 // Why doesn't this code work? Try to make it work and guess what the result will be
 
 // ORIGINAL CODE:
-const numberVariable = 0;
+// const numberVariable = 0;
+
+// numberVariable++;
+// numberVariable++;
+// numberVariable++;
+
+// console.log(numberVariable);
+
+// SOLUTION:
+let numberVariable = 0; // replace const with let
 
 numberVariable++;
 numberVariable++;
 numberVariable++;
 
-console.log(numberVariable);
+console.log(numberVariable); // will output 3
