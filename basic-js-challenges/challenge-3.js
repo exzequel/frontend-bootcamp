b623c88f58c60532208f5a57948f57d08363635a
Exequel <exql.adzn@gmail.com>
2023-02-24 12:36:24 +0800

    add challenge-3 question

---
+++ basic-js-challenges/challenge-3.js
@@ -0,0 +1,9 @@
+// CHALLENGE 3
+// What do 'resultVariable' equal? What data type is it?
+
+const variable1 = 20;
+const variable2 = '40';
+
+const resultVariable = variable1 + variable2;
+console.log(resultVariable);
+
